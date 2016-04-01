f983b0d6304ecbf9c526cb430023b278363a2325
JeroenSormani <jeroen.sormani@gmail.com>
2016-04-01 16:40:27 +0200

    Code consistency changes for includes/payments/actions.php

--- includes/payments/actions.php
+++ includes/payments/actions.php
@@ -270,13 +270,13 @@ function edd_update_payment_backwards_compat( $meta_id, $object_id, $meta_key, $
 	switch( $meta_key ) {
 
 		case '_edd_payment_meta':
-			$meta_value   = maybe_unserialize( $meta_value );
+			$meta_value = maybe_unserialize( $meta_value );
 
 			if( ! isset( $meta_value['tax'] ) ){
 				return;
 			}
 
-			$tax_value    = $meta_value['tax'];
+			$tax_value = $meta_value['tax'];
 
 			$data         = array( 'meta_value' => $tax_value );
 			$where        = array( 'post_id'  => $object_id, 'meta_key' => '_edd_payment_tax' );
