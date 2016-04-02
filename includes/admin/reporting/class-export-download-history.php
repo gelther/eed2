2741d10ce4754e443809989345e4f7aa7012bf8a
JeroenSormani <jeroen.sormani@gmail.com>
2016-04-02 14:48:37 +0200

    Code consistency changes for includes/admin/reporting/class-export-download-history.php

--- includes/admin/reporting/class-export-download-history.php
+++ includes/admin/reporting/class-export-download-history.php
@@ -49,7 +49,7 @@ class EDD_Download_History_Export extends EDD_Export {
 		nocache_headers();
 		header( 'Content-Type: text/csv; charset=utf-8' );
 		header( 'Content-Disposition: attachment; filename=' . apply_filters( 'edd_download_history_export_filename', 'edd-export-' . $this->export_type . '-' . $month . '-' . $year ) . '.csv' );
-		header( "Expires: 0" );
+		header( 'Expires: 0' );
 	}
 
 
