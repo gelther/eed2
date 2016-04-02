6065e8c9bdeba9f0b739b6692b09c4b9255a8c45
JeroenSormani <jeroen.sormani@gmail.com>
2016-04-02 19:17:07 +0200

    Code consistency changes for includes/admin/settings/register-settings.php

--- includes/admin/settings/register-settings.php
+++ includes/admin/settings/register-settings.php
@@ -7,7 +7,7 @@
  * @copyright   Copyright (c) 2015, Pippin Williamson
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       1.0
-*/
+ */
 
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
@@ -144,7 +144,7 @@ function edd_get_settings() {
  *
  * @since 1.0
  * @return void
-*/
+ */
 function edd_register_settings() {
 	if ( false == get_option( 'edd_settings' ) ) {
 		add_option( 'edd_settings' );
@@ -215,7 +215,7 @@ add_action( 'admin_init', 'edd_register_settings' );
  *
  * @since 1.8
  * @return array
-*/
+ */
 function edd_get_registered_settings() {
 	/**
 	 * 'Whitelisted' EDD settings, filters are provided for each settings
