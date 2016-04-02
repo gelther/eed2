785b195a96911158e75e42fd09f2535f5ab8b2e4
JeroenSormani <jeroen.sormani@gmail.com>
2016-04-02 14:18:57 +0200

    Code consistency changes for includes/admin/payments/payments-history.php

--- includes/admin/payments/payments-history.php
+++ includes/admin/payments/payments-history.php
@@ -32,17 +32,17 @@ function edd_payment_history_page() {
 		$payments_table->prepare_items();
 	?>
 	<div class="wrap">
-		<h2><?php echo $edd_payment->labels->menu_name ?></h2>
+		<h2><?php echo $edd_payment->labels->menu_name; ?></h2>
 		<?php do_action( 'edd_payments_page_top' ); ?>
 		<form id="edd-payments-filter" method="get" action="<?php echo admin_url( 'edit.php?post_type=download&page=edd-payment-history' ); ?>">
 			<input type="hidden" name="post_type" value="download" />
 			<input type="hidden" name="page" value="edd-payment-history" />
 
-			<?php $payments_table->views() ?>
+			<?php $payments_table->views(); ?>
 
 			<?php $payments_table->advanced_filters(); ?>
 
-			<?php $payments_table->display() ?>
+			<?php $payments_table->display(); ?>
 		</form>
 		<?php do_action( 'edd_payments_page_bottom' ); ?>
 	</div>
