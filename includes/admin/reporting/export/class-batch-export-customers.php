b242ec9a78f630077061bf60de92af4e347f20a4
JeroenSormani <jeroen.sormani@gmail.com>
2016-04-02 11:27:15 +0200

    Code consistency changes for includes/admin/reporting/export/class-batch-export-customers.php

--- includes/admin/reporting/export/class-batch-export-customers.php
+++ includes/admin/reporting/export/class-batch-export-customers.php
@@ -166,8 +166,8 @@ class EDD_Batch_Customers_Export extends EDD_Batch_Export {
 	 */
 	public function set_properties( $request ) {
 		$this->start    = isset( $request['start'] )            ? sanitize_text_field( $request['start'] ) : '';
-		$this->end      = isset( $request['end']  )             ? sanitize_text_field( $request['end']  )  : '';
-		$this->download = isset( $request['download']         ) ? absint( $request['download']         )   : null;
+		$this->end      = isset( $request['end'] )             ? sanitize_text_field( $request['end'] )  : '';
+		$this->download = isset( $request['download'] ) ? absint( $request['download'] )   : null;
 		$this->price_id = ! empty( $request['edd_price_option'] ) && 0 !== $request['edd_price_option'] ? absint( $request['edd_price_option'] )   : null;
 	}
 }
