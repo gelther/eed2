8a65ee5356c41915f19ae1ff53ea107b3a73a480
JeroenSormani <jeroen.sormani@gmail.com>
2016-04-02 11:27:07 +0200

    Code consistency changes for includes/admin/reporting/class-export-download-history.php

--- includes/admin/reporting/class-export-download-history.php
+++ includes/admin/reporting/class-export-download-history.php
@@ -44,7 +44,7 @@ class EDD_Download_History_Export extends EDD_Export {
 		}
 
 		$month = isset( $_POST['month'] ) ? absint( $_POST['month'] ) : date( 'n' );
-		$year  = isset( $_POST['year']  ) ? absint( $_POST['year']  ) : date( 'Y' );
+		$year  = isset( $_POST['year'] ) ? absint( $_POST['year'] ) : date( 'Y' );
 
 		nocache_headers();
 		header( 'Content-Type: text/csv; charset=utf-8' );
