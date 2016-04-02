ad7062b585667d8ccbae95e6b8bb5d4c8bef365c
JeroenSormani <jeroen.sormani@gmail.com>
2016-04-02 19:17:29 +0200

    Code consistency changes for includes/api/class-edd-api.php

--- includes/api/class-edd-api.php
+++ includes/api/class-edd-api.php
@@ -693,7 +693,7 @@ class EDD_API {
 	 * @since 1.5.1
 	 * @param array $args Arguments to override defaults
 	 * @return array $dates
-	*/
+	 */
 	public function get_dates( $args = array() ) {
 		$dates = array();
 
