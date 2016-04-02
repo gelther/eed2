270071b0f888dfae03c30ad461526b1b8329a6d1
JeroenSormani <jeroen.sormani@gmail.com>
2016-04-02 14:58:11 +0200

    Code consistency changes for includes/admin/reporting/class-api-requests-logs-list-table.php

--- includes/admin/reporting/class-api-requests-logs-list-table.php
+++ includes/admin/reporting/class-api-requests-logs-list-table.php
@@ -144,7 +144,7 @@ class EDD_API_Request_Log_Table extends WP_List_Table {
 			$error   = get_post_field( 'post_content', $item['ID'] );
 			echo '<p><strong>' . __( 'API Request:', 'easy-digital-downloads' ) . '</strong></p>';
 			echo '<div>' . $request . '</div>';
-			if( ! empty( $error ) ) {
+			if ( ! empty( $error ) ) {
 				echo '<p><strong>' . __( 'Error', 'easy-digital-downloads' ) . '</strong></p>';
 				echo '<div>' . esc_html( $error ) . '</div>';
 			}
@@ -192,15 +192,15 @@ class EDD_API_Request_Log_Table extends WP_List_Table {
 				// This is an email search
 				$userdata = get_user_by( 'email', $search );
 
-				if( $userdata ) {
+				if ( $userdata ) {
 					$search = $userdata->ID;
 				}
 
 				$key = '_edd_log_user';
-			} elseif( strlen( $search ) == 32 ) {
+			} elseif ( strlen( $search ) == 32 ) {
 				// Look for an API key
 				$key = '_edd_log_key';
-			} elseif( stristr( $search, 'token:' ) ) {
+			} elseif ( stristr( $search, 'token:' ) ) {
 				// Look for an API token
 				$search = str_ireplace( 'token:', '', $search );
 				$key    = '_edd_log_token';
@@ -208,7 +208,7 @@ class EDD_API_Request_Log_Table extends WP_List_Table {
 				// This is (probably) a user ID search
 				$userdata = get_userdata( $search );
 
-				if( $userdata ) {
+				if ( $userdata ) {
 					$search = $userdata->ID;
 				}
 
