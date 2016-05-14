406e09464a74c32d0692c39f9149a86a0608a7c9
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-14 22:04:14 +0200

    Code consistency changes for includes/admin/tools/class-edd-tools-recount-store-earnings.php

--- includes/admin/tools/class-edd-tools-recount-store-earnings.php
+++ includes/admin/tools/class-edd-tools-recount-store-earnings.php
@@ -205,7 +205,7 @@ class EDD_Tools_Recount_Store_Earnings extends EDD_Batch_Export {
 	 *
 	 * @since  2.5
 	 * @param  string $key   The option_name
-	 * @param  mixed  $value  The value to store
+	 * @param  mixed  $value The value to store
 	 * @return void
 	 */
 	private function store_data( $key, $value ) {
