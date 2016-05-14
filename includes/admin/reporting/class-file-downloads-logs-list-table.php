dde8c6f6ec54ac5c1b72aedafd4ae1bdf72e8f9a
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-14 22:03:40 +0200

    Code consistency changes for includes/admin/reporting/class-file-downloads-logs-list-table.php

--- includes/admin/reporting/class-file-downloads-logs-list-table.php
+++ includes/admin/reporting/class-file-downloads-logs-list-table.php
@@ -75,8 +75,8 @@ class EDD_File_Downloads_Log_Table extends WP_List_Table {
 	 * @since 1.4
 	 * @access public
 	 *
-	 * @param string $text Label for the search box
-	 * @param string $input_id ID of the search box
+	 * @param  string $text     Label for the search box
+	 * @param  string $input_id ID of the search box
 	 *
 	 * @return void
 	 */
@@ -116,10 +116,10 @@ class EDD_File_Downloads_Log_Table extends WP_List_Table {
 	 * @access public
 	 * @since 1.4
 	 *
-	 * @param array $item Contains all the data of the log item
-	 * @param string $column_name The name of the column
+	 * @param  array  $item        Contains all the data of the log item
+	 * @param  string $column_name The name of the column
 	 *
-	 * @return string Column Name
+	 * @return string              Column Name
 	 */
 	public function column_default( $item, $column_name ) {
 		switch ( $column_name ) {
